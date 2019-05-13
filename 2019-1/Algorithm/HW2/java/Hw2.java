681847aac318be8a99b14a2192116b9bdb3e0f76
Peace-Song <driveshoot@snu.ac.kr>
2019-05-14 00:55:21 +0900

    Algorithm/HW2

--- 2019-1/Algorithm/HW2/java/Hw2.java
+++ 2019-1/Algorithm/HW2/java/Hw2.java
@@ -2,18 +2,44 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-public class Hw2{
-    static ArrayList < Integer > A;
+public class Hw2{ // insert red black tree instead of arraylist
+    static ArrayList < Integer > A; // int in [1, 9999] and initially tree empty
+    
     public static void init(){
+        A = new ArrayList<Integer>();
     }
-    public static int osInsert(int x){ 
+    
+    public static int osInsert(int x){ // returns x if x not in tree, 0 otherwise
+    
     }
-    public static int osDelete(int x){
+    
+    public static int osDelete(int x){ // returns x if x in tree, 0 otherwise
+    
     }
-    public static int osSelect(int i){
+    
+    public static int osSelect(int i){ // returns i-th smallest int in tree if |T|>=i, 0 otherwise
+    
     }
-    public static int osRank(int x){
+    
+    public static int osRank(int x){ // returns the rank of x if x in tree, 0 otherwise
+    
     }
+    
     public static boolean check(int[] opt_seq, int[] in_seq, int[] out_seq, int n){
+    
     }
+}
+
+class RBT{
+    void RBT(){}
+    private Node root;
+    public static Node leftRotate(Node x);
+    public static Node rightRotate(Node x); 
+}
+
+class Node{
+    private boolean color; // 0: black, 1: red
+    public boolean getColor(){return color;}
+    public void setColor(boolean color){this.color = color;}
+
 }
