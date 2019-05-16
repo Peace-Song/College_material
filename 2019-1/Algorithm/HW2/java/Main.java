2303bfd1bc0642eb0712fa1ea9da989773c17cd3
Peace-Song <driveshoot@snu.ac.kr>
2019-05-16 15:54:01 +0900

    Algorithm/HW2

--- 2019-1/Algorithm/HW2/java/Main.java
+++ 2019-1/Algorithm/HW2/java/Main.java
@@ -16,8 +16,8 @@ public class Main{
         
         Hw2.init();
 
-        int[] opt_seq2 = { 0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 0, 2, 3, 0, 0, 1, 1, 0 };
-        int[] in_seq2  = { 1, 1, 1, 2, 1, 2, 3, 4, 5, 3, 6, 4, 2, 3, 7, 1, 7, 1 };
+        int[] opt_seq2 = { 0, 1, 0, 0, 2, 3, 0, 0,  0, 1, 0, 0,  0,  0, 0, 0, 0, 0 };
+        int[] in_seq2  = { 1, 1, 1, 2, 1, 2, 3, 11, 5, 3, 6, 10, 12, 3, 7, 9, 4, 8 };
         int[] out_seq2 = new int[opt_seq2.length];
         
         for(int i=0;i<opt_seq2.length;i++){
@@ -33,12 +33,7 @@ public class Main{
             else if(opt_seq2[i]==3){
                 out_seq2[i] = Hw2.osRank(in_seq2[i]);
             }
-
-            Hw2.rbtree.print();
-
         }
-
-            
 
         if(Hw2.check(opt_seq2,in_seq2,out_seq2,opt_seq2.length)){
             System.out.println("correct");
