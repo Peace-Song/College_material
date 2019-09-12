de5d76ff5d6fef6d0e615bcc88c33a94af1a8def
Peace-Song <driveshoot@snu.ac.kr>
2019-09-12 11:10:51 +0900

    corrected form.js

--- 2019-2/SWPP/hw1/javascript/form.js
+++ 2019-2/SWPP/hw1/javascript/form.js
@@ -9,7 +9,18 @@ class Form {
       age,
       birth_month,
       birth_day,
-      birth_year) {}
+      birth_year) {
+        this.email = email;
+        this.password = password;
+        this.password_confirmation = password_confirmation;
+        this.phone_number = phone_number;
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+        this.birth_month = birth_month;
+        this.birth_day = birth_day;
+        this.birth_year = birth_year;
+        }
     // TODO: You may fill in functions in the class.
 
   }
@@ -19,7 +30,14 @@ class Form {
   but.onclick = function() {
       var email = document.forms["form"]["email"].value;
       // TODO: Fill in the rest of the function. Use the Form class defined above
+      console.log("email == %s", email);
+      var email_pattern = /^[^ \t\n\r\f\v@]+@[^ \t\n\r\f\v@.]+[.][a-zA-Z]{2,3}$/;
+      var email_ok = email_pattern.test(email);
+      console.log(email_ok ? "email in pattern" : "email NOT in patern");
 
+      var password = document.forms["form"]["password"].value;
+      var password_patern = /^[a-zA]{8,}$/;
+    
       var form;
 
       let alertMessage = '';
