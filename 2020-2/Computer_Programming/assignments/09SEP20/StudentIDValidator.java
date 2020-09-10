33b94dec6715792d224cf639e8f203e9ecc4e47e
Peace-Song <ddaengjun@gmail.com>
2020-09-10 18:23:40 +0900

    10ESP20 done CP assignment 1

---
+++ 2020-2/Computer_Programming/assignments/09SEP20/StudentIDValidator.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class StudentIDValidator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String studentID;
+
+        while (true) {
+            studentID = scanner.nextLine();
+            
+            if (studentID.equals("exit")) break;
+            
+            validateStudentID(studentID);
+        }
+
+        scanner.close();
+    }
+
+    private static void validateStudentID (String studentID) {
+        if (isProperLength(studentID) && hasProperDivision(studentID) && hasProperDigits(studentID))
+            System.out.println(studentID + " is a valid student ID.");
+
+        return;
+    }
+
+    private static boolean isProperLength (String studentID) {
+        if (studentID.length() != 10) {
+            System.out.println("The input length should be 10.");
+
+            return false;
+        }
+        
+        return true;
+    }
+
+    private static boolean hasProperDivision (String studentID) {
+        if (studentID.charAt(4) != '-') {
+            System.out.println("Fifth character should be '-'.");
+
+            return false;
+        }
+        
+        return true;
+    }
+
+    private static boolean hasProperDigits (String studentID) {
+        for (int idx = 0; idx < studentID.length(); idx++) {
+            if (idx == 4) continue;
+
+            if (!('0' <= studentID.charAt(idx) && studentID.charAt(idx) <= '9')) {
+                System.out.println("Contains an invalid digit.");
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
