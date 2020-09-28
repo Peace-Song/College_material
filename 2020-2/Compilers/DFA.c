28b3a02403f62b63e1e1df164294e0a165800cbc
Peace-Song <ddaengjun@gmail.com>
2020-09-29 01:17:30 +0900

    29SEP20 minor DFA update

--- 2020-2/Compilers/DFA.c
+++ 2020-2/Compilers/DFA.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
+#define MAX_INPUT_LEN 50
+
 int move_table[4][2] = {{1, 0}, {1, 2}, {1, 3}, {1, 0}};
 
 int DFA(char *input, int s0, int (*move)(int, char));
 int move(int s, char c);
 
 int main() {
-  int f = DFA("aababb", 0, move);
+  int f;
+  char input[MAX_INPUT_LEN];
 
-  if (f == 3) printf("accept\n");
-  else printf("nope\n");
+  while (1) {
+    scanf("%s", input);
+
+    f = DFA(input, 0, move);
+
+    if (f == 3) printf("accept string %s\n", input);
+    else printf("nope\n");
+  }
 
   return 0;
 }
